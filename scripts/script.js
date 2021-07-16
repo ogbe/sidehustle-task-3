4571ce74096c3d4f0f84a0e185a9c089c2364b8b
ogbe <51173262+ogbe@users.noreply.github.com>
2021-07-16 07:11:43 +0100

    ft-adds-sidehustle-task-3

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const counter = document.querySelector(".counter");
+        const message = document.querySelector(".message");
+        const addCount = document.querySelector(".increase");
+        const lowerCount = document.querySelector(".decrease");
+
+        window.addEventListener("load", () => {
+            counter.innerHTML = (0).toString();
+        })
+
+        addCount.addEventListener("click", () => {
+            if(counter.innerHTML === (50).toString()) {
+                message.innerHTML = `You have reached the end of the counter.`;
+            } else {
+                counter.innerHTML++;
+                message.innerHTML = "";
+            }
+        })
+
+        lowerCount.addEventListener("click", () => {
+            if(counter.innerHTML === (0).toString()) {
+                message.innerHTML = `You have reached the beginning of the counter.`;
+            } else {
+                counter.innerHTML--;
+                message.innerHTML = "";
+            }
+        })
